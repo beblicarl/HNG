749abe9d385555052a832fe5e2b4093aa90d88db
Carl Bebli <beblicarl.cb@gmail.com>
2022-10-27 08:18:08 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const PORT = 3000
+
+
+app.get('/', (req, res) => {
+    res.send({ "slackUsername": "beblicarl", "backend": true, "age": 28, "bio": "Backend developer" })
+  })
+
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT ${PORT}`)
+  })
